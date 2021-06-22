a952dc778715d6accca56013b630b8c5b8f26b1c
Jose Ameneiro <jameneiroc@gmail.com>
2021-06-22 13:39:10 -0400

    Update Base URLs for requests

--- src/services/googleServices.js
+++ src/services/googleServices.js
@@ -1,4 +1,12 @@
-import http from '../config/axios'
+import axios from 'axios';
+import { GOOGLE } from '../config/api-constants';
+
+const http = axios.create({
+  baseURL: GOOGLE,
+  headers: {
+    "Content-type": "application/json"
+  }
+});
 
 const create = data => {
   return http.post("/scraper/run", data);
@@ -9,7 +17,6 @@ const get = id => {
 };
 
 export default {
- 
   create,
   get
 };
--- src/services/linkedinServices.js
+++ src/services/linkedinServices.js
@@ -1,8 +1,16 @@
-import http from '../config/axiosLinkedin'
+import axios from 'axios';
+import { LINKEDIN } from '../config/api-constants';
+
+const http = axios.create({
+  baseURL: LINKEDIN,
+  headers: {
+    "Content-type": "application/json"
+  }
+});
 
 const createGoo = data => {
-  return http.post("/scraper/runFromGoogle", {body: data});
-};  
+  return http.post("/scraper/runFromGoogle", { body: data });
+};
 
 const create = data => {
   return http.post("/scraper/run", data);
@@ -13,9 +21,7 @@ const get = id => {
 }
 
 export default {
- 
   create,
   createGoo,
   get
-  
 };
